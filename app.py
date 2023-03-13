e46184820243615bff9306db6f47d044eff59d61
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-03-13 11:29:48 +0930

    write session data to db

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 # configure SQL database to store data
 db = SQL("sqlite:///surftrack.db")
 
+ratings = [1, 2, 3, 4, 5]
+
 #initial version - assume only one user - add login feature if time capacity
 
 # define index route
@@ -20,9 +22,6 @@ db = SQL("sqlite:///surftrack.db")
 def index():
     # user route via GET - present form to record session
     if request.method == 'GET':
-
-        #set user rating options
-        ratings = [1, 2, 3, 4, 5]
 
         #get saved spots
         locations = []
@@ -39,40 +38,34 @@ def index():
         user_rating = request.form.get('user_rating')
         user_text = request.form.get('user_text')
 
+        #validate user input
+        if not user_rating:
+            return apology("select valid rating")
+        
+        if not location:
+            return apology('add valid location')
+        
+        #could also check if location in db. should be captured by the drop down.
+
+        if not user_text:
+            user_text = ''
+
         #query db surf_spots for surfline reference
         surfline_spot_ref = db.execute("SELECT surfline_spot_id FROM surf_spots WHERE nickname = ?", location)
 
         # call lookup function
-        session_data = lookup_forecast(surfline_spot_ref)
+        session_data = lookup_forecast(surfline_spot_ref[0]['surfline_spot_id'])
 
         #query db surf_spots for surf_spots(spot_id)
         session_spot_id = db.execute("SELECT spot_id FROM surf_spots WHERE nickname = ?", location)
-
+        session_spot_id = session_spot_id[0]['spot_id']
         #get current time
         session_time = datetime.now()
 
         #remember surf session data in db
-        values = (
-        session_time,
-        session_spot_id,
-        session_data['surf_max'],
-        session_data['surf_min'],
-        session_data['surf_humanRelation'],
-        session_data['primary_swell_height'],
-        session_data['primary_swell_period'],
-        session_data['primary_swell_direction'],
-        session_data['wind_speed'],
-        session_data['wind_direction'],
-        session_data['wind_direction_type'],
-        session_data['wind_gust'],
-        session_data['tide_height'],
-        user_rating,
-        user_text
-        )
-
         
         db.execute("""INSERT INTO sessions (
-                    datetime
+                    datetime,
                     spot_id,
                     surf_max,
                     surf_min,
@@ -88,13 +81,26 @@ def index():
                     user_rating,
                     user_text
                 )
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", values)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""",
+                session_time,
+                session_spot_id,
+                session_data['surf_max'],
+                session_data['surf_min'],
+                session_data['surf_humanRelation'],
+                session_data['primary_swell_height'],
+                session_data['primary_swell_period'],
+                session_data['primary_swell_direction'],
+                session_data['wind_speed'],
+                session_data['wind_direction'],
+                session_data['wind_direction_type'],
+                session_data['wind_gust'],
+                session_data['tide_height'],
+                user_rating,
+                user_text)
         
         return redirect('/')
 
     
-
-
 @app.route("/add_surf_spot", methods=['GET', 'POST'])
 def add_surf_spot():
     # access route via GET
@@ -126,6 +132,17 @@ def add_surf_spot():
         db.execute("INSERT INTO surf_spots (nickname, surfline_spot_id) VALUES (?, ?)", nickname, surfline_spot_id)
         
         return redirect("/")
+    
+@app.route("/history")
+def history():
+    '''user to view saved data'''
+    #get data from db for all records
+    record_data = db.execute("SELECT * FROM sessions JOIN sessions(spot_id) ON surf_spots(spot_id);")
+    
+
+
+    return render_template("history.html")
+
 
 
 
